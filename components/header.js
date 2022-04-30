8663f135f568734e73f6777d276f632106c71abf
komal-tiwari-dev <atulzv1999@gmail.com>
2022-05-01 02:38:14 +0530

    commit

---
+++ components/header.js
@@ -0,0 +1,13 @@
+function header() {
+  return `<h3><a href="index.html">Home</a></h3>
+    <div id="options">
+    <h3><a href="jewelery.html">Jewelery</a></h3>
+    <h3><a href="electronic.html">Electronic</a></h3>
+    <h3><a href="login.html">Login</a></h3>
+    <h3><a href="signup.html">Signp</a></h3>
+    </div>
+    
+    `;
+}
+
+export default header;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import header from "../components/header.js"
+// console.log("Header :",header)
+import '../style/header.css'
+let header_div=document.querySelector("#header")
+header_div.innerHTML=header()
